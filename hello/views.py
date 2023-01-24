597cfb45c430d03464f637e93e36cd3c54c5d157
John Harb <johnharb@Johns-Air-2.lan>
2023-01-23 21:15:30 -0600

    signin/up error handling

--- hello/views.py
+++ hello/views.py
@@ -4,16 +4,33 @@ from django.views import View
 from django.http import JsonResponse
 from hello.models import *
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+import re
+
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+def checkemail(email):
+    if(re.fullmatch(regex, email)):
+        return True
+    else:
+        return False
 
 class signUp(View):
   def get(self, request):
    return render(request,'hello/signup.html')
-  
+
   def post(self, request):
     fname = request.POST["fname"]
     lname = request.POST["lname"]
     email = request.POST["email"]
     password = request.POST["password"]
+
+    if not checkemail(str(email)):
+      messages.error(request, 'invalid email format')
+      return redirect('/')
+    if len(User.objects.filter(email=email)) > 0:
+      messages.error(request, 'account with that email already exists')
+      return redirect('/')
     user = User.objects.create_user(username = email, email = email, first_name = fname, last_name = lname, password = password)
     user.save()
     login(request, user)
@@ -30,6 +47,7 @@ class signIn(View):
     if user is not None:
         login(request, user)
         return redirect('/home/')
+    messages.error(request, "invalid credentials")
     return redirect('/signin/')
  
 class home(View):
