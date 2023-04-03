1528009d7e3af8ea339daa283da84167b404a425
Milad <mmasoodi@uwm.edu>
2023-04-03 17:50:55 -0500

    re added addtank to url

--- web_project/urls.py
+++ web_project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("home/", views.home.as_view()),
     path("profile/", views.profile.as_view()),
     path("tankhome/", views.tankHome.as_view()),
+    path("addtank/", views.addTank.as_view()),
     path("aboutus/", views.aboutUs.as_view()),
     path("signout/", views.signOut.as_view()),
     #path("tankmanage/", views.tankManage.as_view()),
