429ba3dce8cf91d5becef6caeb32064741fe2ffd
Milad <mmasoodi@uwm.edu>
2023-04-27 14:15:00 -0500

    added text notification tests, added param enable/disable checking to check_log_data, added get_dict_of_enabled function to Parameters, added test for get_dict_of_enabled

--- hello/models.py
+++ hello/models.py
@@ -6,7 +6,7 @@ from phonenumber_field.modelfields import PhoneNumberField  # library for verify
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
-
+from sms import send_sms
 
 # send_mail(
 #     'Subject here',
@@ -43,14 +43,13 @@ def save_user_profile(sender, instance, **kwargs):
 
 
 class Tank(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # many to one\
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
 
-    # required, but optional to user, will use generated name if they don't enter one
     name = models.CharField(max_length=20)
     types = [
         ('Fr', 'Freshwater'),
         ('Sa', 'Saltwater'),
-    ]  # may split or add more types later
+    ] 
 
     type = models.CharField(max_length=2, choices=types)
     volume = models.IntegerField()
@@ -58,64 +57,19 @@ class Tank(models.Model):
     send_notifications = models.BooleanField(default=True)
     last_notification_time = models.DateTimeField(blank=True, null=True)
 
-    # one to one relationship to parameters model
+    parameters = models.OneToOneField('Parameters', on_delete=models.CASCADE, null=True, related_name='tank_for_parameters')
 
     def __str__(self):
         return '%s\'s tank: %s' % (self.user.email, self.name)
 
-
-class LogData(models.Model):
-    tank = models.ForeignKey(Tank, on_delete=models.CASCADE)  # many to one
-    types = [
-        ('te', 'temp'),
-        ('ph', 'ph'),
-        ('sa', 'salinity'),
-        ('am', 'ammonia'),
-    ]
-    type = models.IntegerField(choices=types)
-    value = models.DecimalField(max_digits=7, decimal_places=3)  # actual recorded number
-    time_stamp = models.DateTimeField()  # should come from user input or Rpi
-    is_manual = models.BooleanField(default=False)
-
-
-    def __str__(self):
-        return 'Tank %s data: %s, %s, %s' % (self.tank.pk, self.type, self.value, self.time_stamp.isoformat())
-
-# 1 notification per day per tank
-@receiver(post_save, sender=LogData)  # uses signals to check new data when its model is created.
-def check_log_data(sender, instance: LogData, created, **kwargs):
-    if created:
-        data = instance
-        param_range = data.tank.parameters.get_dict_of_range().get(data.types[data.type][1])
-        profile = data.tank.user.profile
-        today = datetime.date.today().isoformat()
-        last_notification = data.tank.last_notification_time
-        if last_notification != None:
-            last_notification = last_notification.date().isoformat()
-        tank_notifications = data.tank.send_notifications and (not data.is_manual) and last_notification != today
-        if tank_notifications and (data.value < param_range[0] or data.value > param_range[1]): # value is out of bounds
-            if profile.email_notifications:
-                print("when email should be sent")
-                subject = f"AquaWatch: tank, {data.tank.name}, has a parameter out of expected range"
-                message = f"Parameter, {data.types[data.type][1]}, is out of range. \nReported value: {data.value}"
-                send_mail(
-                    subject,
-                    message,
-                    'from@example.com',
-                    [data.tank.user.email],
-                    fail_silently=False,
-                )
-            if profile.phone_notifications:
-                print("when text should be sent")
-            if profile.phone_notifications or profile.email_notifications:
-                data.tank.last_notification_time = datetime.datetime.today()
-
-
-
-
-
 class Parameters(models.Model):
-    tank = models.OneToOneField(Tank, on_delete=models.CASCADE)  # one to one
+    temp_enabled = models.BooleanField(default=True)
+    ph_enabled = models.BooleanField(default=True)
+    salinity_enabled = models.BooleanField(default=True)
+    ammonia_enabled = models.BooleanField(default=True)
+    
+    tank = models.OneToOneField('Tank', on_delete=models.CASCADE, related_name='parameters_for_tank')
+
     # unit: fahrenheit
     temp_max = models.DecimalField(max_digits=7, decimal_places=3, default=0.0)
     temp_min = models.DecimalField(max_digits=7, decimal_places=3, default=0.0)
@@ -144,6 +98,15 @@ class Parameters(models.Model):
         "ammonia": [0.0, 0.0],
     }
 
+    def get_dict_of_enabled(self):
+        param_dict = {  # "type": boolean format
+            "temp": self.temp_enabled,
+            "ph": self.ph_enabled,
+            "salinity": self.salinity_enabled,
+            "ammonia": self.ammonia_enabled,
+        }
+        return param_dict
+
     def get_dict_of_range(self):
         param_dict = {  # "type": [type_min, type_max] format
             "temp": [self.temp_min.__float__(), self.temp_max.__float__()],
@@ -153,7 +116,7 @@ class Parameters(models.Model):
         }
         return param_dict
 
-    # intended to get w/ helper, change, then set w/ helper
+    #intended to get w/ helper, change, then set w/ helper
     def set_dict_of_range(self, param_dict: dict[str, list[float, float]]):
         if list(param_dict.keys()) != list(self.get_dict_of_range().keys()):
             raise ValueError("The dict must match keys and types with get_dict_of_range()")
@@ -170,6 +133,64 @@ class Parameters(models.Model):
     def __str__(self):
         return 'Tank %s Parameters' % (self.tank.pk)
 
+class LogData(models.Model):
+    tank = models.ForeignKey(Tank, on_delete=models.CASCADE)  # many to one
+    types = [
+        ('te', 'temp'),
+        ('ph', 'ph'),
+        ('sa', 'salinity'),
+        ('am', 'ammonia'),
+    ]
+    type = models.IntegerField(choices=types)
+    value = models.DecimalField(max_digits=7, decimal_places=3)  # actual recorded number
+    time_stamp = models.DateTimeField()  # should come from user input or Rpi
+    is_manual = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return 'Tank %s data: %s, %s, %s' % (self.tank.pk, self.type, self.value, self.time_stamp.isoformat())
+
+# 1 notification per day per tank
+@receiver(post_save, sender=LogData)  # uses signals to check new data when its model is created.
+def check_log_data(sender, instance: LogData, created, **kwargs):
+    if created:
+        data = instance
+        param_range = data.tank.parameters.get_dict_of_range().get(data.types[data.type][1])
+        is_enabled = data.tank.parameters.get_dict_of_enabled().get(data.types[data.type][1])
+        profile = data.tank.user.profile
+        today = datetime.date.today().isoformat()
+        last_notification = data.tank.last_notification_time
+        if last_notification is not None:
+            last_notification = last_notification.date().isoformat()
+        tank_notifications = data.tank.send_notifications and (not data.is_manual) and last_notification != today and is_enabled
+        if tank_notifications and (data.value < param_range[0] or data.value > param_range[1]): # value is out of bounds
+            if profile.phone_notifications or profile.email_notifications:
+                data.tank.last_notification_time = datetime.datetime.today()
+            if profile.email_notifications:
+                print("when email should be sent")
+                subject = f"AquaWatch: tank, {data.tank.name}, has a parameter out of expected range"
+                message = f"Parameter, {data.types[data.type][1]}, is out of range. \nReported value: {data.value}"
+                send_mail(
+                    subject,
+                    message,
+                    'from@example.com',
+                    [data.tank.user.email],
+                    fail_silently=False,
+                )
+            if profile.phone_notifications:
+                print("when text should be sent")
+                message = f"AquaWatch: Tank: {data.tank.name},Parameter: {data.types[data.type][1]}, is out of range. \nReported value: {data.value}"
+                send_sms(
+                    message,
+                    '+12065550100',
+                    [data.tank.user.profile.phone_num],
+                    fail_silently=False
+                )
+
+
+
+
+
 @receiver(post_save, sender=Tank)  # uses signals to create connected param when a tank is created
 def create_tank_params(sender, instance, created, **kwargs):
     if created:
@@ -179,6 +200,9 @@ def create_tank_params(sender, instance, created, **kwargs):
         elif new_param.tank.type == "Sa":
             new_param.set_dict_of_range(Parameters.saltwater_dict)
         new_param.save()
+        instance.parameters = new_param  # Set the parameters field for the Tank instance
+        instance.save()  # Save the Tank instance
+
 
 @receiver(post_save, sender=Tank)
 def save_rank_params(sender, instance, **kwargs):
