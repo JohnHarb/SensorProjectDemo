6db45c73c37dda93cef05a7ffd6a0aebb4fd67ff
zastr <zastro27@uwm.edu>
2023-04-29 10:45:20 -0500

    -PH sensor code revised -takes average -time = every hour -arduino libraries added -Path established for libraries

--- PHDemo.ino
+++ PHDemo.ino
@@ -1,26 +1,31 @@
 #include "DFRobot_PH.h"
 
 #define PH_PIN A1
-float voltage,phValue,temperature = 25;
+const unsigned long oneHour = 1000UL * 60 * 60; //having it take the reading every hour
+float sumPH,voltage,phValue,temperature = 25;//temp is set at 25^c to represent reef temp(temp changes ph slightly) could pull from manual inputs?
+unsigned long lastTaken = 0 - oneHour;
 DFRobot_PH ph;
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop()
 {
-    static unsigned long timepoint = millis();
-    if(millis()-timepoint>1000U){                  //time interval: 1s
-        timepoint = millis();
-        //temperature = readTemperature();         // read your temperature sensor to execute temperature compensation
+    static unsigned long current = millis();
+    if(current - lastTaken >= oneHour){                  //time interval: 1 hour from last taken sample
+        lastTaken += oneHour;                      //establish new last taken sample
+        int count;
+        //temperature = readTemperature();         // if we get this we can reestablish temperature reading
+        for (int i = 0; i < count; i++){
         voltage = analogRead(PH_PIN)/1024.0*5000;  // read the voltage
         phValue = ph.readPH(voltage,temperature);  // convert voltage to pH with temperature compensation
-        Serial.print("temperature:");
-        Serial.print(temperature,1);
-        Serial.print("^C  pH:");
-        Serial.println(phValue,2);
+        sumPH = sumPH + phValue;                   // add new ph reading to sum
+        }
+        float averagePH = (sumPH / (float)count);  // get average from sum
+        Serial.print("pH:");                       
+        Serial.println(averagePH,2);               // print the average (Would this be better as a return for processing the average data)
     }
     ph.calibration(voltage,temperature);           // calibration process by Serail CMD
 }
