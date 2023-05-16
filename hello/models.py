32abe5186d08216ec72aea63e04eeab0d726882b
zastr <zastro27@uwm.edu>
2023-05-16 00:07:28 -0500

    correction to commented section needs approval now

--- hello/models.py
+++ hello/models.py
@@ -6,7 +6,8 @@ from phonenumber_field.modelfields import PhoneNumberField  # library for verify
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
-#from sms import send_sms
+from sms import send_sms
+from django.conf import settings
 
 # send_mail(
 #     'Subject here',
